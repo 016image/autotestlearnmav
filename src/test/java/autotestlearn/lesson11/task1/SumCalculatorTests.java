365e7b840713f7d5e5f7dc382a286ada5cadb6f4
Shizhgin <mkb07380@mail.ru>
2021-09-12 13:58:01 +0400

    update

---
+++ src/test/java/autotestlearn/lesson11/task1/SumCalculatorTests.java
@@ -0,0 +1,16 @@
+package autotestlearn.lesson11.task1;
+
+import lesson11.task1.Calculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SumCalculatorTests {
+
+    @Test
+    @DisplayName("Проверка метода калькулятора: суммирование n чисел")
+    public void sumTest() {
+        Assertions.assertEquals(10, Calculator.calculateAll(1, 2, 3, 4));
+        Assertions.assertEquals(8, Calculator.calculateAll(5, 3));
+    }
+}
